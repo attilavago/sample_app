227fa8c0721bf6ad39b8c25256ee22d6e3887dc5
Attila Vago <attilavago85@gmail.com>
2014-01-06 00:19:55 +0000

    added tests for title and made them slightly dynamic

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,48 @@ describe "Static pages" do
 
 
   describe "Home page" do
-  	it "should have the content 'Sample App'" do
+
+  	it "should have the h1 'Sample App'" do
   		visit '/static_pages/home'
-  		page.should have_content('Sample App')
+  		page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on rails tutorial sample app | Home")
     end
   end
 
 
-describe "Help page" do
-	it "should have the content 'Help'" do
-		visit '/static_pages/help'
-		page.should have_content('Help')
-	end
-end
 
 
-describe "About page" do
-  it "should have the content 'About Us'" do
-    visit '/static_pages/about'
-    page.should have_content('About Us')
+
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+	  it "should have the title 'Help'" do
+		  visit '/static_pages/help'
+		  page.should have_selector('title', :text => "Ruby on rails tutorial sample app | Help")
+	  end
   end
-end
 
 
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on rails tutorial sample app | About Us")
+    end
+  end
 end
